0546cec5124108ef4a7005cdab5d2163a016b2b1
Luis Alejandro Esparragoza Sanchez <esparragoza.ls@gmail.com>
2019-01-08 10:17:37 -0300

    [FIX] Upload missing files

---
+++ RandomUserList/Pods/Target Support Files/Pods-RandomUserList/Pods-RandomUserList-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RandomUserList : NSObject
+@end
+@implementation PodsDummy_Pods_RandomUserList
+@end
---
+++ RandomUserList/Pods/Target Support Files/Pods-RandomUserListUITests/Pods-RandomUserListUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RandomUserListUITests : NSObject
+@end
+@implementation PodsDummy_Pods_RandomUserListUITests
+@end
